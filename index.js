9a0f650ddc2cfad5113effc2e9a954507d664a39
HASNAIN RAZA <139609308+hasnaingithubrit@users.noreply.github.com>
2023-08-28 18:17:21 +0500

    Add files via upload
    ok

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+// Write a program that uses filter to remove all negative numbers from an array of numbers
+// let arr:number[]=[3,-5,7,-2,-8,4];
+// let positiveNumber:number[]=arr.filter((positiveNumber)=>{
+// if(positiveNumber>0){
+//     return positiveNumber;
+// }
+// });
+// console.log(positiveNumber);
+// Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array where each number is multiplied by 2.
+// var arr:number[]=[1,2,3,4,5];
+// var multiply=arr.map(test);
+// function test(multiply:number){
+//     return multiply*2
+// }
+// console.log("multiply by 2 :" +" "+multiply);
+// Given an array of strings ["apple", "banana", "cherry", "date", "grape"], use the filter method to create a new array containing only the fruits with more than 5 characters.
+// let fruit:string[] = ["apple", "banana", "cherry", "date", "grape"];
+// let fruitUpdate = fruit.filter((elem)=>{
+//     if (elem.length > 5){
+//         return elem;
+//     }
+// });
+// console.log(fruitUpdate);
+// Given an array of numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], use the map and filter methods together to create a new array containing the squares of even numbers.
+// let array:number[]= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let updateArray=array.filter((even)=> even%2==0).map((even)=> even**2);
+// console.log(updateArray);
+// Given an array of temperatures in Celsius [0, 10, 20, 30, 40], use the map method to create a new array where each temperature is converted to Fahrenheit using the formula (Celsius * 9/5) + 32.
+// let temperatureCelsius:number[]=[0, 10, 20, 30, 40];
+// let convertedFahrenheit=temperatureCelsius.map((temperature)=>{
+//     return (temperature * 9/5) + 32;
+// });
+// console.log(convertedFahrenheit);
+// Given an array of numbers [3, 6, 9, 12, 15, 18], use the map and filter methods together to create a new array containing the doubled values of odd numbers.
+// let array:number[]=[3, 6, 9, 12, 15, 18];
+// let updateArray=array.filter((elem)=> elem%2 !==0).map((elem)=>elem**2);
+// console.log(updateArray);
+// Given an array of names ["Alice", "Bob", "Charlie", "David", "Emily"], use the forEach method to log each name with an exclamation mark at the end, e.g., "Alice!".
+let nam = ["Alice", "Bob", "Charlie", "David", "Emily"];
+let updateName = nam.forEach((num) => {
+    console.log(num + "!");
+});
